187ee568757b03cfca89ee8fa435c8a46a40f244
Yogendra Jain <jyogendra44@gmail.com>
2026-07-08 18:20:21 +0530

    change in deployment

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -47,7 +47,7 @@ function Login() {
     setError("");
 
     try {
-      const res = await axios.post("${import.meta.env.VITE_API_URL}/api/auth/login", formData);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, formData);
 
       // Save token
       localStorage.setItem("token", res.data.token);
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -65,7 +65,7 @@ function Register() {
 
     try {
       const response = await axios.post(
-        "${import.meta.env.VITE_API_URL}/api/auth/register",
+        `${import.meta.env.VITE_API_URL}/api/auth/register`,
         { ...formData, role }
       );
 
